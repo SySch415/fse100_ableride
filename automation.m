5e6fbb3ee1b6e8ed1b02237da36e812b36e550b9
SySch415 <158008307+SySch415@users.noreply.github.com>
2025-03-20 13:40:00 -0700

    update rest of automation code

--- automation.m
+++ automation.m
@@ -8,6 +8,23 @@ while 1
   if (colorInt == 5)
     brick.MoveMotor('AD', 0);
     pause(1);
-    break;
+    brick.MoveMotor('AD', 50);
+  elseif (colorInt == 2)
+    brick.MoveMotor('AD', 0);
+    brick.beep();
+    pause(1);
+    brick.beep();
+    pause(1);
+    brick.MoveMotor('AD', 50);
+
+  elseif (colorInt == 4)
+    brick.MoveMotor('AD', 0);
+    brick.beep();
+    pause(1);
+    brick.beep();
+    pause(1);
+    brick.beep();
+    pause(1);
+    brick.MoveMotor('AD', 50);
   end
 end
