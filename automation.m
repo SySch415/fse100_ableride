25156a6d9e8ad6b832ebd12e825032b9a028a396
SySch415 <158008307+SySch415@users.noreply.github.com>
2025-03-19 23:29:59 -0700

    update automation.m

--- automation.m
+++ automation.m
@@ -1,11 +1,13 @@
 brick.SetColorMode(1, 2);
-colorInt = brick.ColorCode(1);
+
+brick.MoveMotor('AD', 50);
 
 while 1
-  brick.MoveMotor('AD', 50);
+  colorInt = brick.ColorCode(1);
 
   if (colorInt == 5)
     brick.MoveMotor('AD', 0);
     pause(1);
+    break;
   end
 end
